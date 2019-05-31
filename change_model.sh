b3dc0a8d512077abdf6fecdaed5e3de9ab0ce6cb
Greg Olsen <olsen@cs.stanford.edu>
2019-05-31 13:09:08 -0700

    Delete change_model.sh

--- change_model.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker stop $(docker ps -a -q)
-docker image rm --force $(docker images edge_analyzer -q)
-docker tag $(docker image load --input $1 | cut -c14-) edge_analyzer:latest
-
-
